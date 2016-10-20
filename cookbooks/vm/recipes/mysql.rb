f028fe06d50b23e0e52cc0f2fa5dde9390bead9f
Ken Iseli <Ken.Iseli@zuehlke.com>
2016-10-20 17:55:13 +0200

    added mysql cookbook, not working yet
    Committer: Ken Iseli

--- cookbooks/vm/recipes/mysql.rb
+++ cookbooks/vm/recipes/mysql.rb
@@ -1,11 +1,24 @@
-#node.set['mysql']['server_root_password'] = 'yolo'
-#node.set['mysql']['port'] = '3308'
-#node.set['mysql']['data_dir'] = '/data'
-#node.set['mysql']['version'] = '5'
-##node.set['mysql']['5']['url'] = 'https://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.53-linux2.6-x86_64.tar.gz'
-#node.set['mysql']['5']['version'] = '5.5'
-#node.set['mysql']['5']['checksum'] = 'dad42a17fed34d025cf4f378973b8722 '
+node.set['mysql']['port'] = '3308'
+node.set['mysql']['data_dir'] = '/data'
+node.set['mysql']['version'] = '5'
+node.set['mysql']['5']['url'] = 'https://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.53-linux2.6-x86_64.tar.gz'
+node.set['mysql']['5']['version'] = '5.5'
+node.set['mysql']['5']['checksum'] = 'dad42a17fed34d025cf4f378973b8722 '
 
-include_recipe 'mysql'
+mysql_service 'default' do
+  port '3306'
+  version '5.5'
+  initial_root_password 'changeme'
+  action [:create, :start]
+end
+
+mysql_config 'default' do
+  source 'mysite.cnf.erb'
+  notifies :restart, 'mysql_service[default]'
+  action :create
+end
+
+#include_recipe 'mysql'
+
 #include_recipe 'mysql::server'
 #include_recipe 'mysql::client'
--- cookbooks/vm/recipes/wildfly.rb
+++ cookbooks/vm/recipes/wildfly.rb
@@ -6,5 +6,5 @@ node.set['wildfly']['8']['url'] = '
 http://download.jboss.org/wildfly/8.0.0.Final/wildfly-8.0.0.Final.tar.gz'
 node.set['wildfly']['8.0.0']['checksum'] = '7316100a8dae90a74fb96f9d70d758daee71ebd70d5ed680307082f010d6f3a9'
 node.set['wildfly']['install_java'] = 'false'
-
+node.set['wildfly']['mysql']['enabled'] = 'true'
 include_recipe 'wildfly'
